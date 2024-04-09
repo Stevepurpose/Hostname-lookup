0c9e841bfd06122b706ff0426515a74e4d1cd785
stevepurpose <stephenodogwu83@gmail.com>
2024-04-09 07:22:00 +0100

    code showing server hostname

---
+++ seehost.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    char hostname[256];
+    if(gethostname(hostname, sizeof(hostname))==0){
+        printf("Hostname:%s\n", hostname);
+    } else{
+        perror("gethostname");
+        return 1;
+    }
+    return 0;
+}
